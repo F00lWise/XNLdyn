080c3962a4704cc58cd9448e631df1dfa3ccc316
F00lWise <robin.engel@desy.de>
2022-04-22 10:53:15 +0200

    Added Multiprocessing and integration over a pulse profile.
    Still need to sanity check the results.

--- main.py
+++ main.py
@@ -82,9 +82,9 @@ if __name__ == '__main__':
     pulse_profile_sigma= 2 # µm rms
 
     fluences_photons_nm2, _ = calculate_fluences(Nsteps_r, pulse_energy_max, pulse_profile_sigma) # These are just to get an idea where to calculate Fluences
-    fluences_to_calculate = np.logspace(np.log10(np.min(fluences_photons_nm2)),
-                                        np.log10(np.max(fluences_photons_nm2)),
-                                        N_local_fluences_to_calculate)
+    fluences_simulated = np.logspace(np.log10(np.min(fluences_photons_nm2)),
+                                     np.log10(np.max(fluences_photons_nm2)),
+                                     N_local_fluences_to_calculate)
 
 
     sim_options = dict(t_span=[-40, 60],method='RK45', rtol=1e-6, atol=1e-8, plot = False)
@@ -95,7 +95,7 @@ if __name__ == '__main__':
         mp.set_start_method('spawn')  # may try "fork" or "forkserver" on unix machines
         with Pool(processes=10) as pool:
             tasklist = []
-            """for fluence in fluences_to_calculate:
+            """for fluence in fluences_simulated:
                 tasklist.append(
                     pool.apply_async(run_modified_simulation,
                                      (*(PAR, sim_options, ['I0', ], [fluence, ]),)
@@ -103,11 +103,11 @@ if __name__ == '__main__':
                 )
             """
             tasklist = [pool.apply_async(run_modified_simulation,(*(PAR, sim_options,  ['I0',] , [fluence,]),)
-                                                 ) for fluence in fluences_to_calculate]
+                                                 ) for fluence in fluences_simulated]
             resultlist = [res.get(timeout=60) for res in tasklist]
     else:
         resultlist = []
-        for fluence in fluences_to_calculate:
+        for fluence in fluences_simulated:
             result = run_modified_simulation(PAR, sim_options, changed_parameters = ['I0',], new_values = [fluence,])
             resultlist.append(result)
     print(resultlist)
@@ -117,28 +117,36 @@ if __name__ == '__main__':
     T = tr/inc
 
     plt.figure()
-    plt.plot(fluences_to_calculate,T,'.-', label = 'For one z-stack')
+    plt.plot(fluences_simulated, T, '.-', label ='For one z-stack')
     plt.xlabel('Fluence (photons/nm2)')
     plt.ylabel('Transmission')
     plt.legend(loc = 'lower right')
 
     final_transmissions = np.zeros(N_pulse_energies)
     final_incidence_check = np.zeros(N_pulse_energies)
+    final_consintency_check = np.zeros(N_pulse_energies)
+
     final_pulse_energies = np.linspace(1/N_pulse_energies, 1, N_pulse_energies)* pulse_energy_max
     for ipe, pulse_en in enumerate(final_pulse_energies):
         local_fluences, dA = calculate_fluences(Nsteps_r, pulse_en,
                                                   pulse_profile_sigma)
-        local_transmitted = np.interp(local_fluences, fluences_to_calculate, tr[:,0])
-        local_incidence_check = np.interp(local_fluences, fluences_to_calculate, inc[:,0])
+
+        local_transmitted = np.interp(local_fluences, fluences_simulated, tr[:, 0])
+        local_incidence_check = np.interp(local_fluences, fluences_simulated, inc[:, 0])
+        local_consistency_check = np.interp(local_fluences, fluences_simulated, fluences_simulated)
 
         final_transmissions[ipe] = np.sum(local_transmitted*dA)
-        final_incidence_check[ipe] = np.sum(local_incidence_check*dA)
+        final_incidence_check[ipe] = np.sum(local_incidence_check*dA) # should result equal final_pulse_energies
+        final_consintency_check[ipe] = np.sum(local_consistency_check*dA) # should result equal final_pulse_energies
 
     plt.figure()
-    plt.plot(final_pulse_energies, final_transmissions*850*1e6*PAR.echarge, 'C1.-', label = 'For the entire spot')
-    plt.plot(final_pulse_energies, final_incidence_check*850*1e6*PAR.echarge, 'C2.-', label = 'Incidences reproduced for reference')
+    plt.plot(final_pulse_energies, final_transmissions*850*1e6*PAR.echarge, 'C1.-', label = 'Transmitted')
+    plt.plot(final_pulse_energies, final_consintency_check*850*1e6*PAR.echarge, 'C2x-', label = 'Incident reproduced')
+    plt.plot(final_pulse_energies, final_incidence_check*850*1e6*PAR.echarge, 'C3.-', label = 'Incidences reproduced for reference')
     plt.xlabel('Pulse Energy (J)')
     plt.ylabel('Transmitted photons')
     plt.legend(loc = 'lower left')
 
     plt.show(block = True)
+
+    print('done')
