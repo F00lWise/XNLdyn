d755f3f6154bbea97b91779e736342cc20ecdf90
engelr (office) <robin.engel@desyl.de>
2022-05-10 17:44:32 +0200

    - Implemented secondary electron scattering

--- XNLdyn/params.py
+++ XNLdyn/params.py
@@ -3,23 +3,23 @@ N_photens= 2      # Number of distinct incident resonant photon energies E_i
 N_j = 40          # Number of points E_j with which the valence system is resolved
 
 ## Sample data
-Z = 50 #Sample thickness in nm
+Z = 25 #Sample thickness in nm
 atomic_density = 91.4 # atoms per nm³
 photon_bandwidth = 0.3 # The energy span of the valence band that each resonant energy interacts with. Flat-top. / eV
 temperature = 300 # Kelvin
 
 # Electronic state numbers per atom
 core_states = 2
-total_valence_states = 12
+#total_valence_states = 12
 valence_GS_occupation = 10
 
 DoS_shapefile = './DoSdata.npy'
 
 ## Rates and cross sections
 
-tau_CH = 30       # Core hole lifetime / fs
-tau_th = 2     # Redistribution time of electrons in the VB / fs
-tau_free = 30   # Free electron lifetime
+tau_CH = 10       # Core hole lifetime / fs
+tau_th = 20     # Redistribution time of electrons in the VB / fs
+tau_free = 5   # Free electron lifetime
 lambda_res_Ei = (10, 5)  # Absorptions length of resonant photon energies / nm
 lambda_nonres = 500    # Absorption length of non-resonant photons, assumed equal for all / nm
 
@@ -27,8 +27,12 @@ lambda_nonres = 500    # Absorption length of non-resonant photons, assumed equa
 ## Fermi Energy
 E_f = 850.7 #eV
 
+Energy_axis_max = 80
+Energy_axis_fine_until = 15
+Energy_axis_min = -6
+
 ## Incident photon profile
 I0       = [3,2]   # Pulse energy density in photons per nm²
 t0       = [0,10]   # Arrival time on time-axis / fs
-tdur_sig = [10,10]  # Rms pulse duration / fs
+tdur_sig = [2,5]  # Rms pulse duration / fs
 E_i      = [858,847] # Photon Energies of incident pulses / eV
--- main_zstack.py
+++ main_zstack.py
@@ -15,13 +15,14 @@ if __name__ == '__main__':
     N_pulse_energies = 20
     Nsteps_r = 100
 
-    pulse_energy_J = 1e-10 # J
+    pulse_energy_J = 1e-16 # J
 
-    PAR.I0 = [XNLdyn.photons_per_J(PAR.E_i[0])*pulse_energy_J,]
+    PAR.I0_i = [XNLdyn.photons_per_J(PAR.E_i_abs[0])*pulse_energy_J,]
+    print('Photon numbers per atom for this simulation: ', PAR.I0_i/PAR.atomic_density)
 
     sim = XNLdyn.XNLsim(PAR, DEBUG=True, load_tables=False)
 
-    #PAR.FermiSolver.plot_lookup_tables()
+    PAR.FermiSolver.plot_lookup_tables()
 
     sim_options = dict(t_span=[-30, 40], method='RK45', rtol=1e-3, atol=1e-8, plot=True, return_full_solution=True)
 
@@ -30,4 +31,4 @@ if __name__ == '__main__':
     print('Transmission: ', 100 * transmitted / incident, ' %')
 
 
-    plt.show()
+    plt.show(block = True)
